827d10e7234464c10ff94e6f48e8a33fb74d7a10
nnpvaan <phuongvan0609@gmail.com>
2023-01-03 00:14:46 +0700

    Add Opusmatch model

--- migrations/env.py
+++ migrations/env.py
@@ -5,9 +5,16 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import app.config
+import app.database.config
+import app.database.models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url", str(app.config.config.db.DATABASE_URI))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +24,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = app.database.models.BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,8 +71,7 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+            connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
